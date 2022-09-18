174f1692db5cdb7ea33b8a335d448613e44a0e07
KenHV <yo@kenharris.xyz>
2022-09-18 06:16:54 +0530

    Import arm64 GCC 20220918
    Build completed on: Sun Sep 18 06:16:54 IST 2022
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/aarch64-elf-gcc
    COLLECT_LTO_WRAPPER=/tmp/gcc-arm64/bin/../libexec/gcc/aarch64-elf/12.2.1/lto-wrapper
    Target: aarch64-elf
    Configured with: ../gcc/configure --target=aarch64-elf CFLAGS='-flto -flto-compression-level=10 -O3 -pipe -ffunction-sections -fdata-sections' CXXFLAGS='-flto -flto-compression-level=10 -O3 -pipe -ffunction-sections -fdata-sections' --disable-decimal-float --disable-docs --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx --disable-nls --disable-shared --enable-default-ssp --enable-languages=c --enable-threads=posix --prefix=/tmp/cirrus-ci-build/../gcc-arm64 --with-glibc-version=2.27 --with-gnu-as --with-gnu-ld --with-headers=/usr/include --with-linker-hash-style=gnu --with-newlib --with-pkgversion='Eva GCC' --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib zstd
    gcc version 12.2.1 20220917 (Stable) (Eva GCC)
    
    LLD: LLD 14.0.6 (compatible with GNU linkers)
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- lib/gcc/aarch64-elf/12.2.1/plugin/include/plugin-version.h
+++ lib/gcc/aarch64-elf/12.2.1/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "12.2.1";
-static char datestamp[] = "20220910";
+static char datestamp[] = "20220917";
 static char devphase[] = "Stable";
 static char revision[] = "";
 
