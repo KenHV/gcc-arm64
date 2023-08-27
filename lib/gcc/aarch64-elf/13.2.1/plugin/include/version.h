6c3141a14b850b68d204233896fcea9ff997ef5c
KenHV <yo@kenharris.xyz>
2023-08-27 07:05:57 +0530

    Import arm64 GCC 20230827
    Build completed on: Sun Aug 27 07:05:57 IST 2023
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/aarch64-elf-gcc
    COLLECT_LTO_WRAPPER=/tmp/gcc-arm64/bin/../libexec/gcc/aarch64-elf/13.2.1/lto-wrapper
    Target: aarch64-elf
    Configured with: ../gcc/configure --target=aarch64-elf --disable-decimal-float --disable-docs --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --enable-default-ssp --enable-languages=c --enable-threads=posix --prefix=/tmp/cirrus-ci-build/../gcc-arm64 --with-gnu-as --with-gnu-ld --with-headers=/usr/include --with-linker-hash-style=gnu --with-newlib --with-pkgversion='Eva GCC' --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib zstd
    gcc version 13.2.1 20230827 (Stable) (Eva GCC)
    
    LLD: LLD 16.0.6 (compatible with GNU linkers)
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- lib/gcc/aarch64-elf/13.2.1/plugin/include/version.h
+++ lib/gcc/aarch64-elf/13.2.1/plugin/include/version.h
@@ -9,7 +9,7 @@
 BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
 Makefile.  */
 
-#define version_string "13.2.1 20230819 (Stable)"
+#define version_string "13.2.1 20230827 (Stable)"
 #define pkgversion_string "(Eva GCC) "
 
 /* This is the location of the online document giving instructions for
