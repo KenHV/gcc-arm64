4ec3f2c785e73b65b8ee734fcf83b243eeff9583
KenHV <yo@kenharris.xyz>
2023-10-29 06:15:15 +0530

    Import arm64 GCC 20231029
    Build completed on: Sun Oct 29 06:15:15 IST 2023
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/aarch64-elf-gcc
    COLLECT_LTO_WRAPPER=/tmp/gcc-arm64/bin/../libexec/gcc/aarch64-elf/13.2.1/lto-wrapper
    Target: aarch64-elf
    Configured with: ../gcc/configure --target=aarch64-elf --disable-decimal-float --disable-docs --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --enable-default-ssp --enable-languages=c --enable-threads=posix --prefix=/tmp/cirrus-ci-build/../gcc-arm64 --with-gnu-as --with-gnu-ld --with-headers=/usr/include --with-linker-hash-style=gnu --with-newlib --with-pkgversion='Eva GCC' --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib zstd
    gcc version 13.2.1 20231028 (Stable) (Eva GCC)
    
    LLD: LLD 16.0.6 (compatible with GNU linkers)
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- lib/gcc/aarch64-elf/13.2.1/plugin/include/plugin-version.h
+++ lib/gcc/aarch64-elf/13.2.1/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "13.2.1";
-static char datestamp[] = "20231021";
+static char datestamp[] = "20231028";
 static char devphase[] = "Stable";
 static char revision[] = "";
 
